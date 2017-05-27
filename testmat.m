30479fef4f308680e7bd270246be4d789aa36ad1
Gene Harvey <gharveymn@gmail.com>
2017-05-27 14:51:50 -0500

    adds testmat for easy creation of test matrices

--- testmat.m
+++ testmat.m
@@ -1,7 +1,11 @@
 function mat = testmat(sz)
 	%TESTMAT returns a square matrix of form [1,2,3;4,5,6;7,8,9]
 	
-	mat = reshape(1:sz^2,[sz,sz])';
+	if(nargin == 0)
+		mat = [1,2,3;4,5,6;7,8,9];
+	else
+		mat = reshape(1:sz^2,[sz,sz])';
+	end
 	
 end
 
