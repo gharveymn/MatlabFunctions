ea28e0c90ee725d01f9db830bfec18f280f6df02
Gene Harvey <gharveymn@gmail.com>
2017-06-08 16:14:39 -0500

    discovery in the biharmonic file

--- biharmonic2.m
+++ biharmonic2.m
@@ -5,24 +5,29 @@ function bih = biharmonic2(xsz,ysz,h,bcx,bcy)
 	%h		the consistence difference between grids
 	Dxx = sptoeplitz([-2 1],xsz)./h.^2;
 	Dyy = sptoeplitz([-2 1],ysz)./h.^2;
-	Cx = sparse([1,xsz],[1,xsz],[(2/h^4),(2/h^4)],xsz,xsz);
-	Cy = sparse([1,ysz],[1,ysz],[(2/h^4),(2/h^4)],ysz,ysz);
+	Cx = sparse([1,xsz],[1,xsz],[(1/h^4),(1/h^4)],xsz,xsz);
+	Cy = sparse([1,ysz],[1,ysz],[(1/h^4),(1/h^4)],ysz,ysz);
 	
 	Dx4 = kron(speye(ysz),Dxx^2) + kron(speye(ysz),Cx);
 	Dy4 = kron(Dyy^2,speye(xsz)) + kron(Cy,speye(xsz));
-	Dx2y2 = kron(Dxx,Dyy);
+	Dx2y2 = kron(Dxx + Cx,Dyy + Cy);
 	
 	if(exist('bcx','var'))
-		Dx4 = ~bcx.*Dx4 + spdiag(bcx);
-		Dx2y2 = ~bcx.*Dx4;
+		Dx4 = spdiag(~bcx)*Dx4 + spdiag(bcx);
+		
+		%why??                v
+		Dx2y2 = spdiag(~bcx)*Dx2y2;
 	end
 	
 	if(exist('bcy','var'))
-		Dy4 = ~bcy.*Dy4 + spdiag(bcy);
-		Dx2y2 = ~bcy.*Dy4;
+		Dy4 = spdiag(~bcy)*Dy4 + spdiag(bcy);
+		
+		%why??                v
+		Dx2y2 = spdiag(~bcy)*Dx2y2;
 	end
 	
-	bih = Dx4 + 2*Dx2y2 + Dy4;
+	%bih = Dx4 + 2*Dx2y2 + Dy4;
+	bih = Dx4 + Dy4;
 	
 	
 end
