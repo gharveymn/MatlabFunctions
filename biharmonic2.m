151a57560e5a233ce9c7fd8785731902b46b738d
Gene Harvey <gharveymn@gmail.com>
2017-06-08 20:48:23 -0500

    fixes bug in bih2

--- biharmonic2.m
+++ biharmonic2.m
@@ -5,12 +5,12 @@ function bih = biharmonic2(xsz,ysz,h,bcx,bcy)
 	%h		the consistence difference between grids
 	Dxx = sptoeplitz([-2 1],xsz)./h.^2;
 	Dyy = sptoeplitz([-2 1],ysz)./h.^2;
-	Cx = sparse([1,xsz],[1,xsz],[(1/h^4),(1/h^4)],xsz,xsz);
-	Cy = sparse([1,ysz],[1,ysz],[(1/h^4),(1/h^4)],ysz,ysz);
+	Cx = sparse([1,xsz],[1,xsz],[(2/h^4),(2/h^4)],xsz,xsz);
+	Cy = sparse([1,ysz],[1,ysz],[(2/h^4),(2/h^4)],ysz,ysz);
 	
 	Dx4 = kron(speye(ysz),Dxx^2) + kron(speye(ysz),Cx);
 	Dy4 = kron(Dyy^2,speye(xsz)) + kron(Cy,speye(xsz));
-	Dx2y2 = kron(Dxx + Cx,Dyy + Cy);
+	Dx2y2 = kron(Dyy,Dxx);
 	
 	if(exist('bcx','var'))
 		Dx4 = spdiag(~bcx)*Dx4 + spdiag(bcx);
@@ -26,8 +26,8 @@ function bih = biharmonic2(xsz,ysz,h,bcx,bcy)
 		Dx2y2 = spdiag(~bcy)*Dx2y2;
 	end
 	
-	%bih = Dx4 + 2*Dx2y2 + Dy4;
-	bih = Dx4 + Dy4;
+	bih = Dx4 + 2*Dx2y2 + Dy4;
+	%bih = Dx4 + Dy4;
 	
 	
 end
